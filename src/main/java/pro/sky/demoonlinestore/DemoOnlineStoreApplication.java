d808132a820b7f930a881ca9f0545316b990f5d7
Hyp <otlichnik56@yandex.ru>
2022-09-11 19:57:41 +0500

    Commit

---
+++ src/main/java/pro/sky/demoonlinestore/DemoOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.demoonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOnlineStoreApplication.class, args);
+	}
+
+}
