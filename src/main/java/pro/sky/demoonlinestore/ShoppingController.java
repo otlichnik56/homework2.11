f3b504f96ff7e5323b167c03189799473c214779
Hyp <otlichnik56@yandex.ru>
2022-09-13 18:01:30 +0500

    Second commit

--- src/main/java/pro/sky/demoonlinestore/ShoppingController.java
+++ src/main/java/pro/sky/demoonlinestore/ShoppingController.java
@@ -1,27 +1,32 @@
 package pro.sky.demoonlinestore;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@Scope(scopeName = "session")
+
 public class ShoppingController {
     private final ShoppingService shoppingService;
 
     public ShoppingController(ShoppingService shoppingService) {
+
         this.shoppingService = shoppingService;
     }
 
     @RequestMapping(path = "/store/order/add")
-    public String addProducts(@RequestParam("idProduct") Integer idProduct) {
+    public String addProducts(@RequestParam("idProduct") List<Integer> idProduct) {
         shoppingService.addProducts(idProduct);
         return "Товор успешно добавлен";
     }
 
     @RequestMapping(path = "/store/order/get")
-    public ArrayList<Integer> getProducts() {
+    public List<Integer> getProducts() {
         return shoppingService.getProducts();
     }
 
